fa372a4127808dc8ec8b0196e73bbcb356405e2b
Bhaskar Mookerji <mookerji@gmail.com>
2018-03-15 15:38:24 -0700

    Ignore files in random directories.

--- openelex_tools/merge_tables.py
+++ openelex_tools/merge_tables.py
@@ -8,22 +8,43 @@ import pandas as pd
 
 logging.basicConfig(level=os.environ.get('LOG_LEVEL', 'INFO'))
 
-Configuration = namedtuple('Configuration', ['source_directory, filter_string', 'output', ])
+Configuration = namedtuple('Configuration', [
+    'source_directory',
+    'filter_string',
+    'output',
+])
+
 
 def get_files(config):
-    pass
-
-@click.command()
-@click.option('--source_directory')
-@click.option('--filter_string', default='general')
-@click.option('--output', default='stdout')
-def main(source_directory, output):
-    filenames = []
-    for root, dirs, files in os.walk(source_directory, topdown=False):
+    names = []
+    for root, dirs, files in os.walk(config.source_directory, topdown=False):
         for name in files:
             if name.endswith('csv'):
                 full_name = os.path.join(root, name)
-                logging.debug(full_name)
+                if config.filter_string and config.filter_string in name:
+                    names.append(full_name)
+                    logging.debug('Adding: %s' % full_name)
+                else:
+                    logging.debug('Skipping: %s' % full_name)
+    return names
+
+
+def read_frame(filename):
+    return pd.read_csv(filename, infer_datetime_format=True)
+
+
+def is_ok(frame):
+    return True
+
+
+@click.command()
+@click.option('--source_directory')
+@click.option('--filter_string', default='general__precinct')
+@click.option('--output', default='stdout')
+def main(source_directory, filter_string, output):
+    config = Configuration(source_directory, filter_string, output)
+    filenames = get_files(config)
+
 
 if __name__ == '__main__':
     main()
