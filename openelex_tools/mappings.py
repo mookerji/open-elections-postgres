86c3a33e63f3585af3c4e2e1442ce172e0e3c5e6
Bhaskar Mookerji <mookerji@gmail.com>
2018-03-16 13:30:45 -0700

    Mappings

---
+++ openelex_tools/mappings.py
@@ -0,0 +1,640 @@
+PARTY_MAPPINGS =
+       {'dem': None,
+ 'lib': None,
+ 'na': None,
+ 'rep': None,
+ 'np': None,
+ 'con': None,
+ 'gre': None,
+ 'ai': None,
+ 'non': None,
+ 'ind': None,
+
+       'd': None,
+ 'r': None,
+ 'ind.': None,
+ 'i': None,
+ 'inp': None,
+ 'bfa': None,
+ 'cst': None,
+ 'grn': None,
+ 'bla': None,
+ 'bet': None,
+
+       'independent': None,
+ 'democratic party': None,
+ 'republican party': None,
+
+       'libertarian party': None,
+ 'better for america party': None,
+
+       'constitution party': None,
+ 'green party': None,
+ 'republican': None,
+ 'democratic': None,
+
+       'libertarian': None,
+ 'green': None,
+ 'colorado reform': None,
+ 'american constitution': None,
+
+       'concerns of people': None,
+ 'unaffiliated': None,
+ 'socialist party': None,
+
+       'prohibition party': None,
+ 'socialist workers party': None,
+
+       'socialist equality': None,
+ 'non partisan': None,
+ nan: None,
+ 'democrat': None,
+
+       'constitution': None,
+ "heartquake '08": None,
+ 'prohibition': None,
+
+       'socialist workers': None,
+ 'boston tea': None,
+ "america's independent": None,
+
+       'socialism and liberation': None,
+ 'u.s. pacifist': None,
+ 'socialist: None,
+ usa': None,
+
+       'objectivist': None,
+ 'unity': None,
+ 'una': None,
+ 'nr': None,
+ 'acn': None,
+ 'dem/rep': None,
+ 'inr': None,
+
+       'american constitution party': None,
+ 'socialist party usa': None,
+
+       'justice party': None,
+ 'peace and freedom': None,
+ "america's party": None,
+
+       'american third position': None,
+ 'objectivist party': None,
+ 'we the people': None,
+
+       'socialist equality party': None,
+ 'independent / republican': None,
+
+       'unaffiliated / republican': None,
+ 'unity party of colorado': None,
+
+       'approval voting party': None,
+ 'american delta party': None,
+
+       'veterans party of america': None,
+ 'independent american party': None,
+
+       'kotlikoff for president': None,
+ 'nonviolent resistance/pacifist': None,
+
+       'independent people of colorado': None,
+ 'american solidarity party': None,
+
+       'nutrition party': None,
+ 'green / democratic': None,
+
+       'unaffiliated / libertarian': None,
+ 'lbt': None,
+ 'jus': None,
+ 'write-in': None,
+
+       'petitioning candidate': None,
+ 'open party': None,
+ 'independent party': None,
+
+       'the bottom line party': None,
+ 'working families party': None,
+
+       'independance party': None,
+ 'bob quast for term limits': None,
+ 'iowa party': None,
+
+       'new independent party iowa': None,
+ 'iowa green party': None,
+ 'oth': None,
+ 'dem.': None,
+
+       'rep.': None,
+ 'w/i': None,
+ 'lib.': None,
+ 'nat.': None,
+ 'ref.': None,
+ 'ust.': None,
+ 'nat': None,
+ 'amh': None,
+ 'ref': None,
+
+       'w/i ind': None,
+ 'con.': None,
+ 'ind. wi': None,
+ 'dem. wi': None,
+ 'uni': None,
+ 'write in': None,
+
+       'write-ins': None,
+ 'reform': None,
+ 'write - in': None,
+ 'amd': None,
+ 'adp': None,
+ 'gop': None,
+
+       'donald trump': None,
+ 'hillary clinton': None,
+ 'gary johnson': None,
+
+       'rocky roque de la fuente': None,
+ 'jill stein': None,
+ 'evan mcmullin': None,
+
+       'rand paul': None,
+ 'jim gray': None,
+ 'harold "hal" rogers': None,
+ 'ray jones': None,
+
+       'william scott wells': None,
+ 'hubert collins': None,
+ 's. brett guthrie': None,
+
+       'stephen l. meredith': None,
+ 'leslie j. stith': None,
+ 'bart rowland': None,
+ 'o': None,
+ 'g': None,
+
+       'l': None,
+ 'n': None,
+ 'other': None,
+ 'nopty': None,
+ 'unenrolled': None,
+
+       'timesizing not downsizing': None,
+ '(write-in)': None,
+
+       'independent progressive': None,
+ 'green-rainbow': None,
+ 'inter. 3rd party': None,
+
+       'america first party': None,
+ 'united independent': None,
+ 'pirate': None,
+ 'npa': None,
+ 'nlp': None,
+
+       'ust': None,
+ 'wcp': None,
+ 'ip': None,
+ 'dfl': None,
+ 'wi': None,
+ 'gp': None,
+ 'glc': None,
+ 'lmn': None,
+ 'cp': None,
+ 'swp': None,
+
+       'ipl': None,
+ 'prg': None,
+ 'cnl': None,
+ 'american delta': None,
+ 'rr': None,
+ 'non-partisan': None,
+ 'pgp': None,
+
+       'wfp': None,
+ 'pro': None,
+ 'pac': None,
+ 'ipo': None,
+ 'pcg': None,
+ 'pg': None,
+ 'prog': None,
+ 'wf': None,
+ 'nav': None,
+
+       'moderate': None,
+ 'independence party o': None,
+ 'pet': None,
+ 'unc': None,
+ 'wfm': None,
+ 'amr': None,
+
+       'amc': None,
+ 'wfw': None,
+ 'ame': None,
+ 'total': None,
+ 'james hart': None,
+ 'rosalind kurita': None,
+
+       'thomas f. kovach': None,
+ 'june griffin': None,
+ 'james lee': None,
+ 'basil marceaux': None,
+
+       'aaron lee childress "skeet"': None,
+ 'melanie hollis': None,
+ 'chris polsen': None,
+
+       'teresa christie': None,
+ 'james e. travis: None,
+ jr.': None,
+ 'timothy b. mcdonald': None,
+
+       'c': None,
+ 'jacob maurer': None,
+ 'richard hall': None,
+ 'jim maynard': None,
+
+       'angelia stinnett': None,
+ 'larry crim': None,
+ 'gary gene davis': None,
+
+       'rachel robinson': None,
+ 'margie nell penn': None,
+ 'christopher schappert': None,
+
+       'lee stewart': None,
+ 'sean puckett': None,
+ 'louie e. johnston jr.': None,
+
+       'jason scott moore': None,
+ 'lady j jean howard-hill': None,
+ 'charles e. drew': None,
+
+       'allen hoenicke': None,
+ 'mike warner': None,
+ 'michael h. thompson': None,
+
+       'mary d. wright': None,
+ 'kimberlee e. smith': None,
+ 'james a. harrell': None,
+
+       'herbert a. bass': None,
+ 'workers world': None,
+ 'independent american': None,
+
+       'liberatrian': None,
+ 'progressive': None,
+ 'liberty union': None,
+ 'justice': None,
+
+       'democratic/progressive': None,
+ 'tea party': None,
+ 'peace and prosperity': None,
+
+       'anti-bushist candidate': None,
+ 'u.s. marijuana': None,
+ 'unknown': None,
+ 'votekiss': None,
+
+       'democratic/republican': None,
+ 'rebpulican/libertarian': None,
+
+       'vermont politically incorrect': None,
+ 'energy independence': None,
+
+       'progressive/democratic': None,
+ 'for your town': None,
+ 'turtle': None,
+
+       'cheap'dem': None,
+ 'lib': None,
+ 'na': None,
+ 'rep': None,
+ 'np': None,
+ 'con': None,
+ 'gre': None,
+ 'ai': None,
+ 'non': None,
+ 'ind': None,
+
+       'd': None,
+ 'r': None,
+ 'ind.': None,
+ 'i': None,
+ 'inp': None,
+ 'bfa': None,
+ 'cst': None,
+ 'grn': None,
+ 'bla': None,
+ 'bet': None,
+
+       'independent': None,
+ 'democratic party': None,
+ 'republican party': None,
+
+       'libertarian party': None,
+ 'better for america party': None,
+
+       'constitution party': None,
+ 'green party': None,
+ 'republican': None,
+ 'democratic': None,
+
+       'libertarian': None,
+ 'green': None,
+ 'colorado reform': None,
+ 'american constitution': None,
+
+       'concerns of people': None,
+ 'unaffiliated': None,
+ 'socialist party': None,
+
+       'prohibition party': None,
+ 'socialist workers party': None,
+
+       'socialist equality': None,
+ 'non partisan': None,
+ nan: None,
+ 'democrat': None,
+
+       'constitution': None,
+ "heartquake '08": None,
+ 'prohibition': None,
+
+       'socialist workers': None,
+ 'boston tea': None,
+ "america's independent": None,
+
+       'socialism and liberation': None,
+ 'u.s. pacifist': None,
+ 'socialist: None,
+ usa': None,
+
+       'objectivist': None,
+ 'unity': None,
+ 'una': None,
+ 'nr': None,
+ 'acn': None,
+ 'dem/rep': None,
+ 'inr': None,
+
+       'american constitution party': None,
+ 'socialist party usa': None,
+
+       'justice party': None,
+ 'peace and freedom': None,
+ "america's party": None,
+
+       'american third position': None,
+ 'objectivist party': None,
+ 'we the people': None,
+
+       'socialist equality party': None,
+ 'independent / republican': None,
+
+       'unaffiliated / republican': None,
+ 'unity party of colorado': None,
+
+       'approval voting party': None,
+ 'american delta party': None,
+
+       'veterans party of america': None,
+ 'independent american party': None,
+
+       'kotlikoff for president': None,
+ 'nonviolent resistance/pacifist': None,
+
+       'independent people of colorado': None,
+ 'american solidarity party': None,
+
+       'nutrition party': None,
+ 'green / democratic': None,
+
+       'unaffiliated / libertarian': None,
+ 'lbt': None,
+ 'jus': None,
+ 'write-in': None,
+
+       'petitioning candidate': None,
+ 'open party': None,
+ 'independent party': None,
+
+       'the bottom line party': None,
+ 'working families party': None,
+
+       'independance party': None,
+ 'bob quast for term limits': None,
+ 'iowa party': None,
+
+       'new independent party iowa': None,
+ 'iowa green party': None,
+ 'oth': None,
+ 'dem.': None,
+
+       'rep.': None,
+ 'w/i': None,
+ 'lib.': None,
+ 'nat.': None,
+ 'ref.': None,
+ 'ust.': None,
+ 'nat': None,
+ 'amh': None,
+ 'ref': None,
+
+       'w/i ind': None,
+ 'con.': None,
+ 'ind. wi': None,
+ 'dem. wi': None,
+ 'uni': None,
+ 'write in': None,
+
+       'write-ins': None,
+ 'reform': None,
+ 'write - in': None,
+ 'amd': None,
+ 'adp': None,
+ 'gop': None,
+
+       'donald trump': None,
+ 'hillary clinton': None,
+ 'gary johnson': None,
+
+       'rocky roque de la fuente': None,
+ 'jill stein': None,
+ 'evan mcmullin': None,
+
+       'rand paul': None,
+ 'jim gray': None,
+ 'harold "hal" rogers': None,
+ 'ray jones': None,
+
+       'william scott wells': None,
+ 'hubert collins': None,
+ 's. brett guthrie': None,
+
+       'stephen l. meredith': None,
+ 'leslie j. stith': None,
+ 'bart rowland': None,
+ 'o': None,
+ 'g': None,
+
+       'l': None,
+ 'n': None,
+ 'other': None,
+ 'nopty': None,
+ 'unenrolled': None,
+
+       'timesizing not downsizing': None,
+ '(write-in)': None,
+
+       'independent progressive': None,
+ 'green-rainbow': None,
+ 'inter. 3rd party': None,
+
+       'america first party': None,
+ 'united independent': None,
+ 'pirate': None,
+ 'npa': None,
+ 'nlp': None,
+
+       'ust': None,
+ 'wcp': None,
+ 'ip': None,
+ 'dfl': None,
+ 'wi': None,
+ 'gp': None,
+ 'glc': None,
+ 'lmn': None,
+ 'cp': None,
+ 'swp': None,
+
+       'ipl': None,
+ 'prg': None,
+ 'cnl': None,
+ 'american delta': None,
+ 'rr': None,
+ 'non-partisan': None,
+ 'pgp': None,
+
+       'wfp': None,
+ 'pro': None,
+ 'pac': None,
+ 'ipo': None,
+ 'pcg': None,
+ 'pg': None,
+ 'prog': None,
+ 'wf': None,
+ 'nav': None,
+
+       'moderate': None,
+ 'independence party o': None,
+ 'pet': None,
+ 'unc': None,
+ 'wfm': None,
+ 'amr': None,
+
+       'amc': None,
+ 'wfw': None,
+ 'ame': None,
+ 'total': None,
+ 'james hart': None,
+ 'rosalind kurita': None,
+
+       'thomas f. kovach': None,
+ 'june griffin': None,
+ 'james lee': None,
+ 'basil marceaux': None,
+
+       'aaron lee childress "skeet"': None,
+ 'melanie hollis': None,
+ 'chris polsen': None,
+
+       'teresa christie': None,
+ 'james e. travis: None,
+ jr.': None,
+ 'timothy b. mcdonald': None,
+
+       'c': None,
+ 'jacob maurer': None,
+ 'richard hall': None,
+ 'jim maynard': None,
+
+       'angelia stinnett': None,
+ 'larry crim': None,
+ 'gary gene davis': None,
+
+       'rachel robinson': None,
+ 'margie nell penn': None,
+ 'christopher schappert': None,
+
+       'lee stewart': None,
+ 'sean puckett': None,
+ 'louie e. johnston jr.': None,
+
+       'jason scott moore': None,
+ 'lady j jean howard-hill': None,
+ 'charles e. drew': None,
+
+       'allen hoenicke': None,
+ 'mike warner': None,
+ 'michael h. thompson': None,
+
+       'mary d. wright': None,
+ 'kimberlee e. smith': None,
+ 'james a. harrell': None,
+
+       'herbert a. bass': None,
+ 'workers world': None,
+ 'independent american': None,
+
+       'liberatrian': None,
+ 'progressive': None,
+ 'liberty union': None,
+ 'justice': None,
+
+       'democratic/progressive': None,
+ 'tea party': None,
+ 'peace and prosperity': None,
+
+       'anti-bushist candidate': None,
+ 'u.s. marijuana': None,
+ 'unknown': None,
+ 'votekiss': None,
+
+       'democratic/republican': None,
+ 'rebpulican/libertarian': None,
+
+       'vermont politically incorrect': None,
+ 'energy independence': None,
+
+       'progressive/democratic': None,
+ 'for your town': None,
+ 'turtle': None,
+
+       'cheap renewable energy': None,
+ 'marijuana': None,
+ 'united states marijuana': None,
+
+       'wri': None,
+ 'pr': None,
+ 'socialism & liberation': None,
+
+       'democratic / working family': None,
+ 'states no party preference': None,
+
+       'independent democrat': None,
+ 'independent gop': None,
+ 'independent republican': None,
+
+       'none' renewable energy': None,
+ 'marijuana': None,
+ 'united states marijuana': None,
+
+       'wri': None,
+ 'pr': None,
+ 'socialism & liberation': None,
+
+       'democratic / working family': None,
+ 'states no party preference': None,
+
+       'independent democrat': None,
+ 'independent gop': None,
+ 'independent republican': None,}
